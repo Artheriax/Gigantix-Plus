ee819a94a6bedee2391394eae9b2cdf45042fb3f
Pablo Hellmann <phtijger@gmail.com>
2025-03-12 08:51:52 +0100

    Added GreaterThanEqualTo, More short number suffixes, optimization of other code.

--- src/Gigantix.lua
+++ src/Gigantix.lua
@@ -1,16 +1,68 @@
 local Gigantix = {}
 
--- Notation table for suffixes used in short notation
+-- Updated notation table using standard short scale abbreviations
 local NOTATION = {
-	'', 'K', 'M', 'B', 'T', 'Qa', 'Qi', 'Sx', 'Sp', 'Oc', 'No', 'Dc',
-	'UD', 'DD', 'TD', 'QaD', 'QiD', 'SxD', 'SpD', 'OcD', 'NnD', 'Vi',
-	'UVg', 'DVg', 'TVg', 'QaVg', 'QiVg', 'SxVg', 'SpVg', 'OcVg', 'NoVg', 'Tg',
-	'Qag', 'Qig', 'Sxg', 'Spg', 'Ocg', 'Nog', 'Ce',
-	'UCe', 'DCe', 'TCe', 'QaCe', 'QiCe', 'SxCe', 'SpCe', 'OtCe', 'NvCe', 'DcCe', 'UDcCe',
-	'GP'
+	'',    -- 10^0 (1)
+	'K',   -- 10^3 (Thousand)
+	'M',   -- 10^6 (Million)
+	'B',   -- 10^9 (Billion)
+	'T',   -- 10^12 (Trillion)
+	'Qa',  -- 10^15 (Quadrillion)
+	'Qi',  -- 10^18 (Quintillion)
+	'Sx',  -- 10^21 (Sextillion)
+	'Sp',  -- 10^24 (Septillion)
+	'Oc',  -- 10^27 (Octillion)
+	'No',  -- 10^30 (Nonillion)
+	'Dc',  -- 10^33 (Decillion)
+	'UD',  -- 10^36 (Undecillion)
+	'DD',  -- 10^39 (Duodecillion)
+	'TD',  -- 10^42 (Tredecillion)
+	'QaD', -- 10^45 (Quattuordecillion)
+	'QiD', -- 10^48 (Quindecillion)
+	'SxD', -- 10^51 (Sedecillion)
+	'SpD', -- 10^54 (Septendecillion)
+	'OcD', -- 10^57 (Octodecillion)
+	'NoD', -- 10^60 (Novendecillion)
+	'Vg',  -- 10^63 (Vigintillion)
+	'UVg', -- 10^66 (Unvigintillion)
+	'DVg', -- 10^69 (Duovigintillion)
+	'TVg', -- 10^72 (Tresvigintillion)
+	'QaVg',-- 10^75 (Quattuorvigintillion)
+	'QiVg',-- 10^78 (Quinvigintillion)
+	'SxVg',-- 10^81 (Sesvigintillion)
+	'SpVg',-- 10^84 (Septemvigintillion)
+	'OcVg',-- 10^87 (Octovigintillion)
+	'NoVg',-- 10^90 (Novemvigintillion)
+	'Tg',  -- 10^93 (Trigintillion)
+	'UTg', -- 10^96 (Untrigintillion)
+	'DTg', -- 10^99 (Duotrigintillion)
+	'TTg', -- 10^102 (Trestrigintillion)
+	'QaTg',-- 10^105 (Quattuortrigintillion)
+	'QiTg',-- 10^108 (Quintrigintillion)
+	'SxTg',-- 10^111 (Sestrigintillion)
+	'SpTg',-- 10^114 (Septentrigintillion)
+	'OcTg',-- 10^117 (Octotrigintillion)
+	'NoTg',-- 10^120 (Noventrigintillion)
+	'Qag', -- 10^123 (Quadragintillion)
+	'Qig', -- 10^153 (Quinquagintillion)
+	'Sxg', -- 10^183 (Sexagintillion)
+	'Spg', -- 10^213 (Septuagintillion)
+	'Ocg', -- 10^243 (Octogintillion)
+	'Nog', -- 10^273 (Nonagintillion)
+	'Ce',  -- 10^303 (Centillion)
+	'UCe', -- 10^306 (Uncentillion)
+	'DCe', -- 10^309 (Duocentillion)
+	'TCe', -- 10^312 (Trecentillion)
+	'QaCe',-- 10^315 (Quattuorcentillion)
+	'QiCe',-- 10^318 (Quincentillion)
+	'SxCe',-- 10^321 (Sexcentillion)
+	'SpCe',-- 10^324 (Septencentillion)
+	'OcCe',-- 10^327 (Octocentillion)
+	'NoCe',-- 10^330 (Novemcentillion)
+	'∞'    -- Infinity (for numbers beyond 10^330)
 }
 
--- Lookup table for suffix multipliers
+-- Lookup table for suffix multipliers (automatically generated)
 local suffixLookup = {}
 for i = 1, #NOTATION do
 	local v = NOTATION[i]
@@ -89,13 +141,14 @@ function Gigantix.getShortNotation(total: {number}): string
 	local num = tonumber(numString) or 0
 	local suffixIndex = math_floor((#numString - 1) / 3) + 1
 
-	if suffixIndex <= 0 or suffixIndex > #NOTATION then
-		return numString
+	-- Handle infinity case
+	if suffixIndex > #NOTATION then
+		return "∞"
 	end
 
 	local divisor = powerCache[suffixIndex - 1] or 1
 	local shortNum = num / divisor
-	local rounded = math_floor(shortNum * 10 + 0.5) / 10
+	local rounded = math_floor(shortNum * 100 + 0.5) / 100
 	local integerPart = math_floor(rounded)
 	local fraction = rounded - integerPart
 
@@ -189,6 +242,36 @@ function Gigantix.subtractLargeNumbers(total: {number}, num: {number}): {number}
 	end
 
 	return result
+end
+
+
+--[[
+	Check if a is greather than or equal to b
+	Example:
+	local total = Gigantix.convertStringToArrayNumber("15000")
+	local num = Gigantix.convertStringToArrayNumber("10000")
+	local isGreaterOrEqual = Gigantix.isGreaterOrEqual(total, num)
+	print(isGreaterOrEqual) -- Output: true
+]]
+function Gigantix.isGreaterOrEqual(a: {number}, b: {number}): boolean
+	-- Compare the lengths of the arrays
+	if #a > #b then
+		return true
+	elseif #a < #b then
+		return false
+	end
+
+	-- Compare each block from the highest to the lowest
+	for i = #a, 1, -1 do
+		if a[i] > b[i] then
+			return true
+		elseif a[i] < b[i] then
+			return false
+		end
+	end
+
+	-- If all blocks are equal, return true
+	return true
 end
 
 return Gigantix
